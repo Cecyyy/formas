675056f9089c7470eb493ba75cc1d1ca8822abc2
mac20_lc3 <cecyparamo94@gmail.com>
2022-05-12 11:38:59 -0500

    creacion del interface con algunos nuevos metodos implementados en las otras clases 12 de mayo trabajo en clase

---
+++ src/AccionesFiguraInterface.java
@@ -0,0 +1,7 @@
+public interface AccionesFiguraInterface {
+    //todos los metodos son abstractos
+    //no es necesario la palabra abstract
+    void  rellenar();
+    void  duplicar();
+    figura hacerPequenia ();
+}
